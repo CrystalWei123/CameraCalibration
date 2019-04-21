5243546af628000f8b0ba5cc1b357f73894e0f69
crystalwei <phes11925@gmail.com>
2019-04-21 21:14:49 +0800

    Add files

--- HW3/run.py
+++ HW3/run.py
@@ -1,16 +1,28 @@
-from typing import List
-import numpy as np
-
 from cv2 import cv2
 
-def sift(img_path: str, str_path: str):
-    img = cv2.imread(img_path)
-    descriptor = cv2.xfeatures2d.SIFT_create()
-    (kps, _) = descriptor.detectAndCompute(img, None)
-    cv2.drawKeypoints(img, kps, img, (0,255,255), flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-    cv2.imwrite(str_path, img)
+from APIS import sift, knnmatch, find_matches, save_matching_img, ransac
 
-if __name__=='__main__':
-    img_path = 'data/1.jpg'
-    str_img = 'results/1.jpg'
-    sift(img_path, str_img)
+
+if __name__ == '__main__':
+    PATH_IMAGES = {
+        "1": ['1.jpg', '2.jpg'],
+        "2": ['hill1.JPG', 'hill2.JPG'],
+        "3": ['S1.jpg', 'S2.jpg'],
+    }
+    for k, img_path in PATH_IMAGES.items():
+        img_list = []
+        kps_list = []
+        des_list = []
+        for path in img_path:
+            img_path = f'./data/{path}'
+            img = cv2.imread(img_path)
+            img_list.append(img)
+            str_img = f'./results/sift/{path}'
+            kps, des = sift(img_path, str_img)
+            kps_list.append(kps)
+            des_list.append(des)
+        rawmatch = knnmatch(des_list)
+        matches = find_matches(rawmatch, 0.8)
+        path = f'./results/lines_knn/{path.split(".")[0]}_result.jpg'
+        save_matching_img(img_list, kps_list, matches, path)
+        ransac(matches, kps_list, 8, 100)
